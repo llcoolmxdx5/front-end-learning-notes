186f5ee7249664af0923c70c9c6ac38fdbb12afb
llcoolmxdx5 <2938129812@qq.com>
2021-05-16 23:09:50 +0800

    WIP:421 findMaximumXOR

---
+++ src/leetcode/0400-0499/0421.findMaximumXOR.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMaximumXOR = function (nums) {};
+
+console.log(findMaximumXOR([3, 10, 5, 25, 2, 8])); // 28
+console.log(findMaximumXOR([0])); // 0
+console.log(findMaximumXOR([2, 4])); // 6
+console.log(findMaximumXOR([8, 10, 2])); // 10
+console.log(findMaximumXOR([14, 70, 53, 83, 49, 91, 36, 80, 92, 51, 66, 70])); // 127
