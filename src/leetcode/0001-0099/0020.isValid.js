0bf824d1eb9d4038e57896721ede48b2ddbfa214
llcoolmxdx5 <2938129812@qq.com>
2021-06-21 09:06:53 +0800

    feat:20-最长有效括号

---
+++ src/leetcode/0001-0099/0020.isValid.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function (s) {
+  if (s.length % 2 !== 0) return false;
+  const map = new Map();
+  map.set("(", ")").set("{", "}").set("[", "]");
+  const stack = [];
+  const arr = s.split("");
+  for (let i = 0; i < arr.length; i++) {
+    const ele = arr[i];
+    if (map.has(ele)) {
+      stack.push(ele);
+    } else {
+      const res = stack.pop();
+      if (map.get(res) !== ele) return false;
+    }
+  }
+  return !stack.length;
+};
+
+console.log(isValid("()")); // true
+console.log(isValid("([)]")); // false
+console.log(isValid("(")); // false
+console.log(isValid("{[]}")); // true
