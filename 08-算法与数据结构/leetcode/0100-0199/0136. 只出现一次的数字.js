e5e30337860fdb22d3278a1b2a15c5f421a0d341
llcoolmxdx5 <2938129812@qq.com>
2021-09-06 23:19:46 +0800

    feat(leetcode):136-只出现一次的数字

---
+++ 08-算法与数据结构/leetcode/0100-0199/0136. 只出现一次的数字.js
@@ -0,0 +1,39 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function (nums) {
+  const set = new Set();
+  nums.forEach((item) => {
+    if (set.has(item)) {
+      set.delete(item);
+    } else {
+      set.add(item);
+    }
+  });
+  return [...set.values()][0];
+};
+
+var singleNumber = function (nums) {
+  const map = new Map();
+  nums.forEach((item) => {
+    map.set(item, (map.get(item) ?? 0) + 1);
+  });
+  for (let [key, value] of map.entries()) {
+    if (value === 1) {
+      return key;
+    }
+  }
+};
+
+var singleNumber = function (nums) {
+  let single = 0;
+  nums.forEach((item) => {
+    single ^= item;
+  });
+  return single;
+};
+
+console.assert(singleNumber([2, 2, 1]) === 1, 1);
+
+console.assert(singleNumber([4, 1, 2, 1, 2]) === 4, 1);
