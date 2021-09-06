f23e1e2c6c66b710d1263e10fa7cb3f2fb002008
llcoolmxdx5 <2938129812@qq.com>
2021-09-06 22:30:10 +0800

    feat(leetcode):191-位 1 的个数

---
+++ 08-算法与数据结构/leetcode/0100-0199/0191. 位1的个数.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number} n - a positive integer
+ * @return {number}
+ */
+var hammingWeight = function (n) {
+  let ret = 0;
+  while (n) {
+    n &= n - 1;
+    ret += 1;
+  }
+  return ret;
+};
+
+var hammingWeight = function (n) {
+  let ret = 0;
+  while (n) {
+    ret += n % 2;
+    n = Math.floor(n/2)
+  }
+  return ret;
+};
+
+console.assert(hammingWeight(0b00000000000000000000000000001011) === 3, 1);
+
+console.assert(hammingWeight(0b00000000000000000000000010000000) === 1, 2);
+
+console.assert(hammingWeight(0b11111111111111111111111111111101) === 31, 3);
