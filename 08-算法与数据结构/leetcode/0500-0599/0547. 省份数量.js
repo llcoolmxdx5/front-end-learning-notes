eca0a0688a35a13f06e40e31520338161a112a9b
llcoolmxdx5 <2938129812@qq.com>
2021-09-24 11:44:15 +0800

    feat(leetcode):547-省份数量

---
+++ 08-算法与数据结构/leetcode/0500-0599/0547. 省份数量.js
@@ -0,0 +1,74 @@
+/**
+ * @param {number[][]} isConnected
+ * @return {number}
+ */
+var findCircleNum = function (isConnected) {
+  const visited = new Set();
+  const m = isConnected.length;
+  const dfs = (i) => {
+    for (let j = 0; j < m; j++) {
+      if (isConnected[i][j] === 1 && !visited.has(j)) {
+        visited.add(j);
+        dfs(j);
+      }
+    }
+  };
+  let step = 0;
+  for (let i = 0; i < m; i++) {
+    if (!visited.has(i)) {
+      step += 1;
+      dfs(i);
+    }
+  }
+  return step;
+};
+
+var findCircleNum = function (isConnected) {
+  const provinces = isConnected.length;
+  const visited = new Set();
+  let circles = 0;
+  const queue = new Array();
+  for (let i = 0; i < provinces; i++) {
+    if (!visited.has(i)) {
+      queue.push(i);
+      while (queue.length) {
+        const j = queue.shift();
+        visited.add(j);
+        for (let k = 0; k < provinces; k++) {
+          if (isConnected[j][k] === 1 && !visited.has(k)) {
+            queue.push(k);
+          }
+        }
+      }
+      circles++;
+    }
+  }
+  return circles;
+};
+
+console.assert(
+  findCircleNum([
+    [1, 1, 0],
+    [1, 1, 0],
+    [0, 0, 1],
+  ]) === 2,
+  1
+);
+console.assert(
+  findCircleNum([
+    [1, 0, 0],
+    [0, 1, 0],
+    [0, 0, 1],
+  ]) == 3,
+  2
+);
+
+console.assert(
+  findCircleNum([
+    [1, 0, 0, 1],
+    [0, 1, 1, 0],
+    [0, 1, 1, 1],
+    [1, 0, 1, 1],
+  ]) === 1,
+  3
+);
