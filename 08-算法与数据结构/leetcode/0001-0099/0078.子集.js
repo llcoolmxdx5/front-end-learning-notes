78b764b84dadb400343ca87c7e540cd0c3250d02
llcoolmxdx5 <2938129812@qq.com>
2021-10-01 17:20:24 +0800

    feat(leetcode):78-子集

---
+++ 08-算法与数据结构/leetcode/0001-0099/0078.子集.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var subsets = function (nums) {
+  const stack = [],
+    ans = [];
+  const backTrack = (start) => {
+    ans.push([...stack])
+    for (let index = start; index < nums.length; index++) {
+      const element = nums[index];
+      stack.push(element)
+      backTrack(index+1)
+      stack.pop()
+    }
+  }
+  backTrack(0)
+  return ans;
+};
+
+console.log(subsets([1, 2, 3])); // [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+
+console.log(subsets([0])); // [[],[0]]
