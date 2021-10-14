450ab296d830a56b8a0956ff74ed6cb24a6c9f10
llcoolmxdx5 <2938129812@qq.com>
2021-10-14 21:02:10 +0800

    feat(leetcode):852-山脉数组的峰顶索引

---
+++ 08-算法与数据结构/leetcode/0800-0899/0852. 山脉数组的峰顶索引.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+var peakIndexInMountainArray = function (arr) {
+  let left = 0,
+    right = arr.length - 1;
+  while (true) {
+    const mid = left + Math.floor((right - left) / 2);
+    if (left === right) {
+      return left;
+    }
+    if (arr[mid] < arr[mid + 1]) {
+      left = mid + 1;
+    } else if (arr[mid] > arr[mid + 1]) {
+      right = mid;
+    }
+  }
+};
+
+console.assert(
+  peakIndexInMountainArray([24, 69, 100, 99, 79, 78, 67, 36, 26, 19]) === 2,
+  1
+);
+
+console.assert(peakIndexInMountainArray([0, 1, 0]) === 1, 2);
+
+console.assert(peakIndexInMountainArray([3, 4, 5, 1]) === 2, 3);
