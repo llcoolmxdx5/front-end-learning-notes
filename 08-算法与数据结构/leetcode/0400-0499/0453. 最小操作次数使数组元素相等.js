b01730245b668773e8c8d65dff109ec74121b07c
llcoolmxdx5 <2938129812@qq.com>
2021-10-21 15:50:35 +0800

    feat(leetcode): 453-最小操作次数使数组元素相等

---
+++ 08-算法与数据结构/leetcode/0400-0499/0453. 最小操作次数使数组元素相等.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minMoves = function (nums) {
+  const min = Math.min(...nums);
+  let resp = 0
+  for (let i = 0; i < nums.length; i++) {
+    resp += nums[i] - min
+  }
+  return resp
+};
+
+console.assert(minMoves([1, 2, 3]) === 3, 1);
+console.assert(minMoves([1, 1, 1]) === 0, 2);
