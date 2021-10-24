9fc399db71b10695d3ff4f0efa1afd46d4ff6b86
llcoolmxdx5 <2938129812@qq.com>
2021-10-25 01:25:04 +0800

    feat(leetcode): 240-搜索二维矩阵II

---
+++ 08-算法与数据结构/leetcode/0200-0299/0240. 搜索二维矩阵II.js
@@ -0,0 +1,66 @@
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+var searchMatrix = function (matrix, target) {
+  const m = matrix.length;
+  const n = matrix[0].length;
+  let row = 0;
+  let col = n - 1;
+  while (true) {
+    // console.log(row, col);
+    if (matrix[row][col] === target) {
+      return true;
+    } else if (matrix[row][col] < target) {
+      row += 1;
+    } else if (matrix[row][col] > target) {
+      col -= 1;
+    }
+    if (col < 0 || row > m - 1) {
+      return false;
+    }
+  }
+};
+
+console.assert(
+  searchMatrix(
+    [
+      [2, 5],
+      [2, 8],
+      [7, 9],
+      [7, 11],
+      [9, 11],
+    ],
+    7
+  ) === true,
+  1
+);
+
+console.assert(
+  searchMatrix(
+    [
+      [1, 4, 7, 11, 15],
+      [2, 5, 8, 12, 19],
+      [3, 6, 9, 16, 22],
+      [10, 13, 14, 17, 24],
+      [18, 21, 23, 26, 30],
+    ],
+    5
+  ) === true,
+  2
+);
+
+console.assert(
+  searchMatrix(
+    [
+      [1, 4, 7, 11, 15],
+      [2, 5, 8, 12, 19],
+      [3, 6, 9, 16, 22],
+      [10, 13, 14, 17, 24],
+      [18, 21, 23, 26, 30],
+    ],
+    20
+  ) === false,
+  3
+);
