87c348c789a668aeddc3c356e8685cf2f7a18230
llcoolmxdx5 <2938129812@qq.com>
2021-10-30 01:17:47 +0800

    feat(leetcode): 260-只出现一次的数字III

---
+++ 08-算法与数据结构/leetcode/0200-0299/0260. 只出现一次的数字III.js
@@ -0,0 +1,37 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var singleNumber = function (nums) {
+  const map = new Map();
+  nums.forEach((x) => map.set(x, 1 + (map.get(x) ?? 0)));
+  const resp = [];
+  for (let [key, value] of map.entries()) {
+    if (value === 1) {
+      resp.push(key);
+    }
+  }
+  return resp;
+};
+
+var singleNumber = function (nums) {
+  if (nums.length === 2) return nums;
+  let xorSum = 0;
+
+  for (const num of nums) {
+    xorSum ^= num;
+  }
+  let type1 = 0,
+    type2 = 0;
+  const lsb = xorSum & -xorSum;
+  for (const num of nums) {
+    if (num & lsb) {
+      type1 ^= num;
+    } else {
+      type2 ^= num;
+    }
+  }
+  return [type1, type2];
+};
+
+console.log(singleNumber([1, 2, 1, 3, 2, 5]));
