6cd066b53bb6bafb43e40f9a324e0889be830393
llcoolmxdx5 <2938129812@qq.com>
2021-10-30 00:46:56 +0800

    feat(leetcode): 9-回文数

---
+++ 08-算法与数据结构/leetcode/0001-0099/0009. 回文数.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function (x) {
+  if (x < 0) return false;
+  if (x < 10) return true;
+  if (x % 10 === 0) return false;
+  let revertedNumber = 0;
+  while (x > revertedNumber) {
+    revertedNumber = revertedNumber * 10 + (x % 10);
+    x = Math.floor(x / 10);
+  }
+  // 当数字长度为奇数时，我们可以通过 revertedNumber/10 去除处于中位的数字。
+  // 例如，当输入为 12321 时，在 while 循环的末尾我们可以得到 x = 12，revertedNumber = 123，
+  // 由于处于中位的数字不影响回文（它总是与自己相等），所以我们可以简单地将其去除。
+  return x === revertedNumber || x === Math.floor(revertedNumber / 10);
+};
+
+console.assert(isPalindrome(121) === true, 1);
+console.assert(isPalindrome(10) === false, 2);
+console.assert(isPalindrome(2222) === true, 3);
