80e6a1c725f2b6810e78b8abcabf09ff24c4c2b9
llcoolmxdx5 <2938129812@qq.com>
2021-11-03 00:47:25 +0800

    feat(leetcode): 139-单词拆分

---
+++ 08-算法与数据结构/leetcode/0100-0199/0139. 单词拆分.js
@@ -0,0 +1,50 @@
+/**
+ * @param {string} s
+ * @param {string[]} wordDict
+ * @return {boolean}
+ */
+var wordBreak = (s, wordDict) => {
+  const { length } = s;
+  let dp = Array(length + 1).fill(false);
+  dp[0] = true;
+  for (let i = 0; i <= length; i++) {
+    for (let j = 0; j < wordDict.length; j++) {
+      if (i >= wordDict[j].length) {
+        if (
+          s.slice(i - wordDict[j].length, i) === wordDict[j] &&
+          dp[i - wordDict[j].length]
+        ) {
+          dp[i] = true;
+        }
+      }
+    }
+  }
+  return dp[length];
+};
+
+var wordBreak = function (s, wordDict) {
+  const set = new Set(wordDict);
+  const maxWordLength = Math.max(...wordDict.map((x) => x.length));
+  const { length } = s;
+  const dp = new Array(length + 1).fill(false);
+  dp[0] = true;
+  for (let i = 1; i <= length; i++) {
+    for (let j = i - 1; j >= 0; j--) {
+      if (i - j > maxWordLength) {
+        break;
+      }
+      if (dp[j] && set.has(s.slice(j, i))) {
+        dp[i] = true;
+        break;
+      }
+    }
+  }
+  return dp[length];
+};
+
+console.assert(wordBreak("leetcode", ["leet", "code"]) === true, 1);
+console.assert(wordBreak("applepenapple", ["apple", "pen"]) === true, 2);
+console.assert(
+  wordBreak("catsandog", ["cats", "dog", "sand", "and", "cat"]) === false,
+  3
+);
