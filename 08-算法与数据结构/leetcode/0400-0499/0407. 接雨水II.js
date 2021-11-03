2e404e1f2a001226286ca0668fdd678743f92b9b
llcoolmxdx5 <2938129812@qq.com>
2021-11-03 22:49:38 +0800

    feat(leetcode): 407-接雨水II

---
+++ 08-算法与数据结构/leetcode/0400-0499/0407. 接雨水II.js
@@ -0,0 +1,96 @@
+class PriorityQueue {
+  constructor() {
+    this.data = [];
+  }
+  isEmpty() {
+    return !this.data.length;
+  }
+  enqueue(item) {
+    this.data.push(item);
+  }
+  dequeue() {
+    let entry = 0;
+    for (let i = 0; i < this.data.length; i++) {
+      if (this.data[i].height < this.data[entry].height) {
+        entry = i;
+      }
+    }
+    return this.data.splice(entry, 1)[0];
+  }
+  getData() {
+    return this.data;
+  }
+}
+/**
+ * @param {number[][]} heightMap
+ * @return {number}
+ */
+var trapRainWater = function (heightMap) {
+  const m = heightMap.length;
+  const n = heightMap[0].length;
+  if (m < 3 || n < 3) {
+    return 0;
+  }
+  let ans = 0;
+  const pq = new PriorityQueue();
+  const visited = new Array(m).fill(0).map((_) => new Array(n).fill(false));
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      if (i === 0 || i === m - 1 || j === 0 || j === n - 1) {
+        visited[i][j] = true;
+        pq.enqueue({ height: heightMap[i][j], row: i, col: j });
+      }
+    }
+  }
+  const dirX = [0, 0, 1, -1];
+  const dirY = [1, -1, 0, 0];
+  while (!pq.isEmpty()) {
+    const { height, row, col } = pq.dequeue();
+    for (let k = 0; k < 4; k++) {
+      const nx = row + dirX[k];
+      const ny = col + dirY[k];
+      if (nx >= 0 && nx < m && ny >= 0 && ny < n && !visited[nx][ny]) {
+        if (heightMap[nx][ny] < height) {
+          ans += height - heightMap[nx][ny];
+        }
+        visited[nx][ny] = true;
+        pq.enqueue({
+          height: Math.max(heightMap[nx][ny], height),
+          row: nx,
+          col: ny,
+        });
+      }
+    }
+  }
+  return ans;
+};
+
+console.assert(
+  trapRainWater([
+    [12, 13, 1, 12],
+    [13, 4, 13, 12],
+    [13, 8, 10, 12],
+    [12, 13, 12, 12],
+    [13, 13, 13, 13],
+  ]) === 14,
+  3
+);
+
+console.assert(
+  trapRainWater([
+    [1, 4, 3, 1, 3, 2],
+    [3, 2, 1, 3, 2, 4],
+    [2, 3, 3, 2, 3, 1],
+  ]) === 4,
+  1
+);
+console.assert(
+  trapRainWater([
+    [3, 3, 3, 3, 3],
+    [3, 2, 2, 2, 3],
+    [3, 2, 1, 2, 3],
+    [3, 2, 2, 2, 3],
+    [3, 3, 3, 3, 3],
+  ]) === 10,
+  2
+);
