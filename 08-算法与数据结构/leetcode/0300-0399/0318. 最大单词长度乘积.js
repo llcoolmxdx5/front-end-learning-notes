5b10bb427e39bae77b151d639f4a2a1d6842588c
llcoolmxdx5 <2938129812@qq.com>
2021-11-17 23:35:35 +0800

    feat(leetcode): 318-最大单词长度乘积

---
+++ 08-算法与数据结构/leetcode/0300-0399/0318. 最大单词长度乘积.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string[]} words
+ * @return {number}
+ */
+var maxProduct = function (words) {
+  const { length } = words;
+  const nums = new Array(length).fill(0);
+  for (let i = 0; i < length; i++) {
+    const word = words[i];
+    const { length: wordLength } = word;
+    for (let j = 0; j < wordLength; j++) {
+      nums[i] |= 1 << (word[j].charCodeAt() - 97);
+    }
+  }
+  let ans = 0;
+  for (let i = 0; i < length; i++) {
+    for (let j = i + 1; j < length; j++) {
+      if ((nums[i] & nums[j]) === 0) {
+        ans = Math.max(ans, words[i].length * words[j].length);
+      }
+    }
+  }
+  // console.log(nums);
+  return ans
+};
+
+console.assert(
+  maxProduct(["abcw", "baz", "foo", "bar", "xtfn", "abcdef"]) === 16,
+  1
+);
