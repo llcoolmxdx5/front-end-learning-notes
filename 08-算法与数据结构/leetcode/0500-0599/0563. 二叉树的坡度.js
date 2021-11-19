b49d47e05c6b26c1e10614ebfaf5c5109c0925a0
llcoolmxdx5 <2938129812@qq.com>
2021-11-19 08:52:57 +0800

    feat(leetcode): 563-二叉树的坡度

---
+++ 08-算法与数据结构/leetcode/0500-0599/0563. 二叉树的坡度.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var findTilt = function (root) {
+  let ans = 0;
+  const dfs = (node) => {
+    if (!node) {
+      return 0;
+    }
+    const sumLeft = dfs(node.left);
+    const sumRight = dfs(node.right);
+    ans += Math.abs(sumLeft - sumRight);
+    return sumLeft + sumRight + node.val;
+  };
+  dfs(root);
+  return ans;
+};
