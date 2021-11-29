19a6d3f3ac02680a295b6262062f9b590dab1bb3
llcoolmxdx5 <2938129812@qq.com>
2021-11-29 17:48:59 +0800

    feat(leetcode): 786-第K个最小的素数分数

---
+++ 08-算法与数据结构/leetcode/0700-0799/0786. 第K个最小的素数分数.js
@@ -0,0 +1,32 @@
+import PriorityQueue from "../../datastructure/Queue/PriorityQueue";
+
+/**
+ * @param {number[]} arr
+ * @param {number} k
+ * @return {number[]}
+ */
+var kthSmallestPrimeFraction = function (arr, k) {
+  const pq = new PriorityQueue(
+    (a, b) => arr[a[0]] * arr[b[1]] < arr[a[1]] * arr[b[0]]
+  );
+  const { length } = arr;
+  for (let i = 1; i < length; i++) {
+    pq.insert([0, i]);
+  }
+  let cur;
+  for (let r = 0; r < k; r++) {
+    cur = pq.remove();
+    const [i, j] = cur;
+    if (i < j - 1) {
+      pq.insert([i + 1, j]);
+    }
+  }
+  return [arr[cur[0]], arr[cur[1]]];
+};
+
+console.assert(kthSmallestPrimeFraction([1, 2, 3, 5], 3).join() === "2,5", 1);
+console.assert(kthSmallestPrimeFraction([1, 7], 1).join() === "1,7", 2);
+console.assert(
+  kthSmallestPrimeFraction([1, 13, 17, 59], 6).join() === "13,17",
+  3
+);
