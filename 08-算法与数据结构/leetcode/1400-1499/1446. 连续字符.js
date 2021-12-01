799515a08efc3d63e72afce277543360d12d33f9
llcoolmxdx5 <2938129812@qq.com>
2021-12-01 08:35:33 +0800

    feat(leetcode): 1446-连续字符

---
+++ 08-算法与数据结构/leetcode/1400-1499/1446. 连续字符.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var maxPower = function (s) {
+  const { length } = s;
+  let power = 0;
+  let temp = 0;
+  for (let i = 0; i < length; i++) {
+    temp += 1;
+    if (s[i + 1] !== s[i]) {
+      power = Math.max(power, temp);
+      temp = 0;
+    }
+  }
+  return power;
+};
+
+console.assert(maxPower("tourist") === 1, 1);
+console.assert(maxPower("t") === 1, 2);
+console.assert(maxPower("hooraaaaaaaaaaay") === 11, 3);
+console.assert(maxPower("hooraaaaaaaaaaa") === 11, 4);
