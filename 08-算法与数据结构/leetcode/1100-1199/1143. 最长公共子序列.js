1a5c536342eae4741e26e1b9dfe6a3b4879ad5e3
llcoolmxdx5 <2938129812@qq.com>
2021-12-02 13:18:37 +0800

    feat(leetcode): 1143-最长公共子序列

---
+++ 08-算法与数据结构/leetcode/1100-1199/1143. 最长公共子序列.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} text1
+ * @param {string} text2
+ * @return {number}
+ */
+const longestCommonSubsequence = (text1, text2) => {
+  const n = text1.length;
+  const m = text2.length;
+  const dp = Array.from({ length: n + 1 }, () => new Array(m + 1).fill(0));
+  for (let i = 1; i <= n; i++) {
+    for (let j = 1; j <= m; j++) {
+      if (text1[i - 1] === text2[j - 1]) {
+        dp[i][j] = dp[i - 1][j - 1] + 1;
+      } else {
+        dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+      }
+    }
+  }
+  return dp[n][m];
+};
+
+console.assert(longestCommonSubsequence("abcde", "ace") === 3, 1);
+console.assert(longestCommonSubsequence("abc", "abc") === 3, 2);
+console.assert(longestCommonSubsequence("abc", "def") === 0, 3);
