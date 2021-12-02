5414429801c4058826984efdaf136969617afe32
llcoolmxdx5 <2938129812@qq.com>
2021-12-02 23:30:54 +0800

    feat(leetcode): 226-翻转二叉树

---
+++ 08-算法与数据结构/leetcode/0200-0299/0226. 翻转二叉树.js
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var invertTree = function (root) {
+  if (!root === null) return root;
+  const queue = [root];
+  while (queue.length) {
+    const item = queue.shift();
+    let temp = item.left;
+    item.left = item.right;
+    item.right = temp;
+    if (item.left !== null) {
+      queue.push(item.left);
+    }
+    if (item.right !== null) {
+      queue.push(item.right);
+    }
+  }
+  return root;
+};
+
+var invertTree = function (root) {
+  if (root === null) return root;
+  let temp = root.left;
+  root.left = root.right;
+  root.right = temp;
+  invertTree(root.left)
+  invertTree(root.right)
+  return root
+}
