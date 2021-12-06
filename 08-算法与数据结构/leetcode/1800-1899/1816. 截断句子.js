4b11a976dfda16f3f5aaf4c1b1a3f330b7d7f3c2
llcoolmxdx5 <2938129812@qq.com>
2021-12-06 16:58:24 +0800

    feat(leetcode): 1816-截断句子

---
+++ 08-算法与数据结构/leetcode/1800-1899/1816. 截断句子.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {string}
+ */
+var truncateSentence = function (s, k) {
+  return s.split(" ").slice(0, k).join(" ");
+};
+
+console.assert(
+  truncateSentence("What is the solution to this problem", 4) ===
+    "What is the solution",
+  1
+);
