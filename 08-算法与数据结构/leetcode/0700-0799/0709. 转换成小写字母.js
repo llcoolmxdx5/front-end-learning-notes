abf892f678278874da63f8153506f94915d74491
llcoolmxdx5 <2938129812@qq.com>
2021-12-12 10:26:51 +0800

    feat(leetcode): 709-转换成小写字母

---
+++ 08-算法与数据结构/leetcode/0700-0799/0709. 转换成小写字母.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+ var toLowerCase = function(s) {
+  let res = ''
+  for(let i = 0, len = s.length; i < len; i++) {
+    if(/[a-zA-Z]/.test(s[i])) {
+      const code = s[i].charCodeAt()
+      res += String.fromCharCode(code | 32)
+    } else {
+      res += s[i]
+    }
+  }
+  return res
+};
