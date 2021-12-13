378fc3daf5181497adc4fca364bca3fb98e66a4e
llcoolmxdx5 <2938129812@qq.com>
2021-12-13 18:30:51 +0800

    feat(leetcode): 807-保持城市天际线

---
+++ 08-算法与数据结构/leetcode/0800-0899/0807. 保持城市天际线.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var maxIncreaseKeepingSkyline = function (grid) {
+  const n = grid.length;
+  const m = grid[0].length;
+  const rowMax = new Array(n).fill(0);
+  const colMax = new Array(m).fill(0);
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      rowMax[i] = Math.max(rowMax[i], grid[i][j]);
+      colMax[j] = Math.max(colMax[j], grid[i][j]);
+    }
+  }
+  let ans = 0;
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      ans += Math.min(rowMax[i], colMax[j]) - grid[i][j];
+    }
+  }
+  return ans;
+};
+
+console.assert(
+  maxIncreaseKeepingSkyline([
+    [3, 0, 8, 4],
+    [2, 4, 5, 7],
+    [9, 2, 6, 3],
+    [0, 3, 1, 0],
+  ]) === 35,
+  1
+);
