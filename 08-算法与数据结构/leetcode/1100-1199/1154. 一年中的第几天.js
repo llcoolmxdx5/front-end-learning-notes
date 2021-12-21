4eba57b714cf074bb991df54949a82568a2cfa46
llcoolmxdx5 <2938129812@qq.com>
2021-12-21 11:13:24 +0800

    feat(leetcode): 1154-一年中的第几天

---
+++ 08-算法与数据结构/leetcode/1100-1199/1154. 一年中的第几天.js
@@ -0,0 +1,31 @@
+/**
+ * @param {string} date
+ * @return {number}
+ */
+var dayOfYear = function (date) {
+  const [year, month, day] = date.split("-").map((item) => parseInt(item));
+  let ans = day;
+  const arr = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+  for (let i = 0; i < month - 1; i++) {
+    ans += arr[i];
+  }
+  if (month > 2) {
+    if (year % 400 === 0 || (year % 4 === 0 && year % 100 !== 0)) {
+      ans += 1;
+    }
+  }
+  return ans;
+};
+
+var dayOfYear = function (str) {
+  let time1, time2;
+  let d = new Date(str);
+  time1 = d.getTime();
+  d.setMonth(0);
+  d.setDate(1);
+  time2 = d.getTime();
+  let day = 24 * 60 * 60 * 1000;
+  let g = time1 - time2;
+  return g / day + 1;
+};
+
