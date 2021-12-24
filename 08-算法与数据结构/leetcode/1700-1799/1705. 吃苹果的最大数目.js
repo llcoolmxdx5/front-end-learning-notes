217847a8e5baae91cb7d72f52617953ceca68e19
llcoolmxdx5 <2938129812@qq.com>
2021-12-25 01:11:13 +0800

    feat(leetcode): 1705-吃苹果的最大数目

---
+++ 08-算法与数据结构/leetcode/1700-1799/1705. 吃苹果的最大数目.js
@@ -0,0 +1,31 @@
+import PriorityQueue from "../../datastructure/Queue/PriorityQueue";
+/**
+ * @param {number[]} apples
+ * @param {number[]} days
+ * @return {number}
+ */
+var eatenApples = function (apples, days) {
+  const pq = new PriorityQueue((a, b) => a[1] < b[1]);
+  let ans = 0;
+  let start = 0;
+  const { length } = apples;
+  while (start < length || !pq.isEmpty()) {
+    if (start < length && apples[start]) {
+      pq.offer([apples[start], days[start] + start]);
+    }
+    while (!pq.isEmpty()) {
+      const [x, y] = pq.poll();
+      if (y === start) {
+        continue;
+      }
+      ans += 1;
+      x > 1 && pq.offer([x - 1, y]);
+      break;
+    }
+    start += 1;
+  }
+  return ans
+};
+
+console.assert(eatenApples([1, 2, 3, 5, 2], [3, 2, 1, 4, 2]) === 7, 1);
+console.assert(eatenApples([3, 0, 0, 0, 0, 2], [3, 0, 0, 0, 0, 2]) === 5, 2);
