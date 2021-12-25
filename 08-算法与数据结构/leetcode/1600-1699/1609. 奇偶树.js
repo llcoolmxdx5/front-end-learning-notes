87a18dca81babab9a600604ad16bb26fb4e57c2b
llcoolmxdx5 <2938129812@qq.com>
2021-12-26 00:17:33 +0800

    feat(leetcode): 1609-奇偶树

---
+++ 08-算法与数据结构/leetcode/1600-1699/1609. 奇偶树.js
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isEvenOddTree = function (root) {
+  let flag = true; // 偶数层 节点都是奇数 递增
+  const queue = [root];
+  while (queue.length) {
+    const { length } = queue;
+    let value = queue[0].val;
+    for (let i = 0; i < length; i++) {
+      const node = queue.shift();
+      if (flag) {
+        if(node.val % 2 === 0) return false
+        if(i > 0 && node.val <= value) return false
+      } else {
+        if(node.val % 2 !== 0) return false
+        if(i > 0 && node.val >= value) return false
+      }
+      value = node.val
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+    flag = !flag;
+  }
+  return true;
+};
