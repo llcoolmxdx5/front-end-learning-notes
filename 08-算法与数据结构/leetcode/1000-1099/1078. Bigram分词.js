57495677c2fb65fa7b091378ad8e7e9a0ebc8ea4
llcoolmxdx5 <2938129812@qq.com>
2021-12-26 10:35:40 +0800

    feat(leetcode): 1078-Bigram分词

---
+++ 08-算法与数据结构/leetcode/1000-1099/1078. Bigram分词.js
@@ -0,0 +1,39 @@
+/**
+ * @param {string} text
+ * @param {string} first
+ * @param {string} second
+ * @return {string[]}
+ */
+var findOcurrences = function (text, first, second) {
+  const arr = text.split(" ");
+  const ans = [];
+  for (let i = 0, len = arr.length; i < len - 2; i++) {
+    if (arr[i] === first && arr[i + 1] === second) {
+      ans.push(arr[i + 2]);
+    }
+  }
+  // console.log(ans);
+  return ans;
+};
+
+console.assert(
+  findOcurrences(
+    "alice is a good girl she is a good student",
+    "a",
+    "good"
+  ).join() === ["girl", "student"].join(),
+  1
+);
+console.assert(
+  findOcurrences(
+    "alice is aa good girl she is a good student",
+    "a",
+    "good"
+  ).join() === ["student"].join(),
+  2
+);
+console.assert(
+  findOcurrences("we will we will rock you", "we", "will").join() ===
+    ["we", "rock"].join(),
+  3
+);
