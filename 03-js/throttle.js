ba4df55ce3a8adce0c50076e5518ef1e536ba72c
llcoolmxdx5 <2938129812@qq.com>
2021-12-30 01:41:13 +0800

    feat(js): throttle

---
+++ 03-js/throttle.js
@@ -0,0 +1,18 @@
+const throttle = (fn, wait) => {
+  let inThrottle, lastFn, lastTime;
+  return function (...args) {
+    if (!inThrottle) {
+      fn.apply(this, args);
+      lastTime = Date.now();
+      inThrottle = true;
+    } else {
+      clearTimeout(lastFn);
+      lastFn = setTimeout(() => {
+        if (Date.now() - lastTime >= wait) {
+          fn.apply(this, args);
+          lastTime = Date.now();
+        }
+      }, Math.max(wait - (Date.now() - lastTime), 0));
+    }
+  };
+};
