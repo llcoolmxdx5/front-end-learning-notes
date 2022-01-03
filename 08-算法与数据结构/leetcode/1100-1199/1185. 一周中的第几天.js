d22c1add88912d3e03656877673a73f2742a69ae
llcoolmxdx5 <2938129812@qq.com>
2022-01-03 14:08:27 +0800

    feat(leetcode): 1185-一周中的第几天

---
+++ 08-算法与数据结构/leetcode/1100-1199/1185. 一周中的第几天.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number} day
+ * @param {number} month
+ * @param {number} year
+ * @return {string}
+ */
+var dayOfTheWeek = function (day, month, year) {
+  const week = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const date = new Date(year, month - 1, day);
+  return week[date.getDay()];
+};
+
+console.assert(dayOfTheWeek(31, 8, 2019) === "Saturday", 1);
