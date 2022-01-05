2ad1d212028e01036d73801caf9893f642900fde
llcoolmxdx5 <2938129812@qq.com>
2022-01-05 18:35:24 +0800

    feat(leetcode): 1576-替换所有的问号

---
+++ 08-算法与数据结构/leetcode/1500-1599/1576. 替换所有的问号.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var modifyString = function (s) {
+  const n = s.length;
+  const arr = [...s];
+  for (let i = 0; i < n; ++i) {
+    if (arr[i] === "?") {
+      for (let j = 0; j < 3; ++j) {
+        if (
+          (i > 0 && arr[i - 1] === String.fromCharCode(97 + j)) ||
+          (i < n - 1 && arr[i + 1] === String.fromCharCode(97 + j))
+        ) {
+          continue;
+        }
+        arr[i] = String.fromCharCode(97 + j);
+        break;
+      }
+    }
+  }
+  return arr.join("");
+};
+
+console.log(modifyString("???????"));
