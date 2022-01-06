bcd286973a0e2fc7e6b13fc0d9d5703f0f7abb39
llcoolmxdx5 <2938129812@qq.com>
2022-01-06 11:42:44 +0800

    feat(leetcode): 71-简化路径

---
+++ 08-算法与数据结构/leetcode/0001-0099/0071. 简化路径.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} path
+ * @return {string}
+ */
+var simplifyPath = function (path) {
+  const names = path.split("/");
+  const stack = [];
+  for (const name of names) {
+    if (name === "..") {
+      if (stack.length) {
+        stack.pop();
+      }
+    } else if (name.length && name !== ".") {
+      stack.push(name);
+    }
+  }
+  // console.log(stack);
+  return "/" + stack.join("/");
+};
+
+console.assert(simplifyPath("/home/") === "/home", 1);
+console.assert(simplifyPath("/../") === "/", 2);
+console.assert(simplifyPath("/home//foo/") === "/home/foo", 3);
+console.assert(simplifyPath("/a/./b/../../c/") === "/c", 4);
