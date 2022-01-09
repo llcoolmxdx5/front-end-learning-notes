0d99ac9c22a0c9a9b11838069ad293ec020b195f
llcoolmxdx5 <2938129812@qq.com>
2022-01-09 10:50:50 +0800

    feat(leetcode): 按键持续时间最长的键

---
+++ 08-算法与数据结构/leetcode/1600-1699/1629. 按键持续时间最长的键.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[]} releaseTimes
+ * @param {string} keysPressed
+ * @return {character}
+ */
+var slowestKey = function (releaseTimes, keysPressed) {
+  let ans = keysPressed[0];
+  let maxTime = releaseTimes[0];
+  for (let i = 1, len = releaseTimes.length; i < len; i++) {
+    const diff = releaseTimes[i] - releaseTimes[i - 1];
+    if (diff > maxTime) {
+      ans = keysPressed[i];
+      maxTime = diff;
+    } else if (diff === maxTime) {
+      if (keysPressed[i] > ans) {
+        ans = keysPressed[i];
+      }
+    }
+  }
+  return ans;
+};
+
+console.assert(
+  slowestKey((releaseTimes = [9, 29, 49, 50]), (keysPressed = "cbcd")) === "c",
+  1
+);
+console.assert(
+  slowestKey((releaseTimes = [12, 23, 36, 46, 62]), (keysPressed = "spuda")) ===
+    "a",
+  2
+);
