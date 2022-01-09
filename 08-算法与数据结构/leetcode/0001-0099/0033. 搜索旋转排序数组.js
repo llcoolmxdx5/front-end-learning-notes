e5a41783411f69b0684ce52a4d102855609f020a
llcoolmxdx5 <2938129812@qq.com>
2022-01-09 12:47:08 +0800

    feat(leetcode): 33-搜索旋转排序数组

--- 08-算法与数据结构/leetcode/0001-0099/0033. 搜索旋转排序数组.js
+++ 08-算法与数据结构/leetcode/0001-0099/0033. 搜索旋转排序数组.js
@@ -3,12 +3,13 @@
  * @param {number} target
  * @return {number}
  */
-var search = function (nums, target) {
-  let left = 0,
-    right = nums.length - 1;
+ var search = function (nums, target) {
+  // [3,4,5,6,0,1,2]
+  // [5,6,0,1,2,3,4]
+  let left = 0;
+  let right = nums.length - 1;
   while (left <= right) {
     const mid = left + Math.floor((right - left) / 2);
-    // console.log(nums[left], nums[mid], nums[right]);
     if (nums[mid] === target) {
       return mid;
     }
@@ -18,21 +19,19 @@ var search = function (nums, target) {
     if (nums[right] === target) {
       return right;
     }
-    if (nums[left] < nums[mid]) {
-      // 顺序
+    if (nums[left] > nums[mid]) {
+      // [mid, r] 有序
+      if (target > nums[mid] && target < nums[right]) {
+        left = mid + 1;
+      } else {
+        right = mid - 1;
+      }
+    } else {
+      // [l, mid] 有序
       if (target > nums[left] && target < nums[mid]) {
         right = mid - 1;
       } else {
         left = mid + 1;
-      }
-    } else {
-      // 乱序
-      if (target > nums[left]) {
-        right = mid - 1;
-      } else if (target < nums[right]) {
-        left += 1;
-      } else {
-        return -1;
       }
     }
   }
@@ -40,9 +39,7 @@ var search = function (nums, target) {
 };
 
 console.assert(search([4, 5, 6, 7, 0, 1, 2], 0) === 4, 1);
-
 console.assert(search([4, 5, 6, 7, 0, 1, 2], 3) === -1, 2);
-
 console.assert(search([1], 0) === -1, 3);
-
 console.assert(search([3, 1], 2) === -1, 4);
+console.assert(search([5, 1, 2, 3, 4], 1) === 1, 5);
