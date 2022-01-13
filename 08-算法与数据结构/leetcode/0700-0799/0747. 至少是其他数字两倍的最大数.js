b7ff4ec079f30de31f64ce363409848f73193838
llcoolmxdx5 <2938129812@qq.com>
2022-01-13 21:18:04 +0800

    feat(leetcode): 747-至少是其他数字两倍的最大数

---
+++ 08-算法与数据结构/leetcode/0700-0799/0747. 至少是其他数字两倍的最大数.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var dominantIndex = function (nums) {
+  const { length } = nums;
+  let first = nums[0];
+  let second = 0;
+  let ans = 0;
+  for (let i = 1; i < length; i++) {
+    const value = nums[i];
+    if (value > first) {
+      second = first;
+      first = value;
+      ans = i
+    } else if (value > second) {
+      second = value;
+    }
+  }
+  if (first / 2 >= second) {
+    return ans;
+  }
+  return -1
+};
+
+console.assert(dominantIndex([3, 6, 1, 0]) === 1, 1);
