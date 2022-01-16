1edb141b2d00c8886cb597a907eb6c32799a8e8d
llcoolmxdx5 <2938129812@qq.com>
2022-01-16 15:43:06 +0800

    feat(js): call

--- 03-js/snippets/call.js
+++ 03-js/snippets/call.js
@@ -8,11 +8,16 @@ const call =
   };
 
 Function.prototype._call = function (context, ...args) {
-  context = context || window;
+  // 按规范非undefined和null 才转化为对象,其他都是全局对象
+  // 严格模式下,不传为 undefined(未实现)
+  context =
+    context !== undefined || context !== null ? Object(context) : globalThis;
+  // 用 Symbol 避免参数重复
   const fn = Symbol("fn");
   context[fn] = this;
-  context[fn](...args);
+  const result = context[fn](...args);
   delete context[fn];
+  return result;
 };
 
 let foo = { value: 1, fn: 1 };
