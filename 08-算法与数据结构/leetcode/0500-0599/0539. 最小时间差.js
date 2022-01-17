1d4c5a120614b144de13d03fa5d4d00f72a1d2d0
llcoolmxdx5 <2938129812@qq.com>
2022-01-18 00:32:42 +0800

    feat(leetcode): 539-最小时间差

---
+++ 08-算法与数据结构/leetcode/0500-0599/0539. 最小时间差.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string[]} timePoints
+ * @return {number}
+ */
+var findMinDifference = function (timePoints) {
+  const arr = [];
+  for (const timePoint of timePoints) {
+    const [hour, minute] = timePoint.split(":").map((str) => parseInt(str));
+    arr.push(hour * 60 + minute);
+  }
+  const { length } = arr;
+  if (length > 1440) {
+    return 0;
+  }
+  arr.sort((a, b) => a - b);
+  let diff = 1440;
+  for (let i = 1; i < length; i++) {
+    diff = Math.min(diff, arr[i] - arr[i - 1]);
+  }
+  return Math.min(diff, arr[0] + 1440 - arr[length - 1]);
+};
+
+console.assert(findMinDifference(["23:59", "00:00"]) === 1, 1);
