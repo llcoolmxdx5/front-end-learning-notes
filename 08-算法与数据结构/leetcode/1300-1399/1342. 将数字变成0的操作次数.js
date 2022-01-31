49bbb2480f9e066f21879243c02d9142ad8b184e
llcoolmxdx5 <2938129812@qq.com>
2022-01-31 21:50:58 +0800

    feat(leetcode): 1342-将数字变成0的操作次数

---
+++ 08-算法与数据结构/leetcode/1300-1399/1342. 将数字变成0的操作次数.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var numberOfSteps = function (num) {
+  let ans = 0;
+  while (num) {
+    if (num % 2 === 0) {
+      num /= 2;
+    } else {
+      num -= 1;
+    }
+    ans += 1;
+  }
+  return ans;
+};
+
+1110
+
+console.assert(numberOfSteps(14) === 6, 1);
+console.assert(numberOfSteps(123) === 12, 2);
