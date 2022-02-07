35e3f877d8633fb50d1d8bdf5757ebd845e35d51
llcoolmxdx5 <2938129812@qq.com>
2022-02-07 15:43:38 +0800

    feat(js): new

---
+++ 03-js/snippets/new.js
@@ -0,0 +1,15 @@
+function New(Func, ...args) {
+  const obj = {};
+  obj.__proto__ = Func.prototype;
+  const ret = Func.apply(obj, args);
+  return typeof ret === "object" ? ret : obj;
+}
+const objA = New(A, 1, 2);
+// equals to
+const newA = new A(1, 2);
+// 更优的做法
+function New(Func, ...args) {
+  const obj = Object.create(Func.prototype);
+  const res = Func.apply(obj, args);
+  return typeof res === "object" ? res : obj;
+}
