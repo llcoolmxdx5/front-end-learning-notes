03bd4d798b86571ca9d10fff5ddf95c4e495f69e
llcoolmxdx5 <2938129812@qq.com>
2022-02-13 17:37:56 +0800

    feat(leetcode): 1189-“气球”的最大数量

---
+++ 08-算法与数据结构/leetcode/1100-1199/1189. “气球”的最大数量.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} text
+ * @return {number}
+ */
+var maxNumberOfBalloons = function (text) {
+  // b a l o n
+  const arr = new Array(5).fill(0);
+  for (const str of text) {
+    if (str === "b") {
+      arr[0] += 1;
+    } else if (str === "a") {
+      arr[1] += 1;
+    } else if (str === "n") {
+      arr[2] += 1;
+    } else if (str === "l") {
+      arr[3] += 0.5;
+    } else if (str === "o") {
+      arr[4] += 0.5;
+    }
+  }
+  return parseInt(Math.min(...arr));
+};
+
+console.assert(maxNumberOfBalloons("loonbalxballpoon") === 2, 1);
