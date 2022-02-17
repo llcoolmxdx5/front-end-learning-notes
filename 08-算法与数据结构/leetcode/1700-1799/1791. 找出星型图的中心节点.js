f6ae99d5afd8f0990a23c0f668dbe1812e534d5c
llcoolmxdx5 <2938129812@qq.com>
2022-02-18 00:10:35 +0800

    feat(leetcode): 1791-找出星型图的中心节点

---
+++ 08-算法与数据结构/leetcode/1700-1799/1791. 找出星型图的中心节点.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[][]} edges
+ * @return {number}
+ */
+ var findCenter = function(edges) {
+  const set = new Set(edges[0])
+  const [a, b] = edges[1]
+  if(set.has(a)) {
+    return a
+  }
+  return b
+};
