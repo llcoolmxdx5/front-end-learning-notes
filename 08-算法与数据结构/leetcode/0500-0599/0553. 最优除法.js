581b578fb333498aa11d5c6761b7263ab29709aa
llcoolmxdx5 <2938129812@qq.com>
2022-02-27 01:10:05 +0800

    feat(leetcode): 553-最优除法

---
+++ 08-算法与数据结构/leetcode/0500-0599/0553. 最优除法.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {string}
+ */
+var optimalDivision = function (nums) {
+  if (nums.length === 1) {
+    return String(nums[0]);
+  }
+  if (nums.length === 2) {
+    return nums.join("/");
+  }
+  let ans = nums[0] + "/(";
+  return ans + nums.slice(1).join("/") + ")";
+};
