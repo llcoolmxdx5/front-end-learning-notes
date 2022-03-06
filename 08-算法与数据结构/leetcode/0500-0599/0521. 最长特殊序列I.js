98d63396e4a154df4f81311b393bae60fe16f9ac
llcoolmxdx5 <2938129812@qq.com>
2022-03-06 11:59:09 +0800

    feat(leetcode): 521-最长特殊序列Ⅰ

---
+++ 08-算法与数据结构/leetcode/0500-0599/0521. 最长特殊序列I.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} a
+ * @param {string} b
+ * @return {number}
+ */
+var findLUSlength = function (a, b) {
+  if (a.length === b.length) {
+    if (a === b) {
+      return -1;
+    }
+    return a.length;
+  }
+  return Math.max(a.length, b.length);
+};
+
+console.assert(findLUSlength("aba", "cdc") === 3, 1);
+console.assert(findLUSlength("aaa", "bbb") === 3, 2);
+console.assert(findLUSlength("aaa", "aaa") === -1, 3);
