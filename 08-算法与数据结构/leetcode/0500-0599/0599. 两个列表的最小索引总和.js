c30f6d0e1b2698cb076742692fdbb3b90292ef79
llcoolmxdx5 <2938129812@qq.com>
2022-03-14 08:50:16 +0800

    feat(leetcode): 599-两个列表的最小索引总和

---
+++ 08-算法与数据结构/leetcode/0500-0599/0599. 两个列表的最小索引总和.js
@@ -0,0 +1,45 @@
+/**
+ * @param {string[]} list1
+ * @param {string[]} list2
+ * @return {string[]}
+ */
+var findRestaurant = function (list1, list2) {
+  let index = Number.MAX_SAFE_INTEGER;
+  let ans = [];
+  const map = new Map(list1.map((item, i) => [item, i]));
+  for (let i = 0, length = list2.length; i < length; i++) {
+    const element = list2[i];
+    if (!map.has(element)) {
+      continue;
+    }
+    const sum = map.get(element) + i;
+    if (sum < index) {
+      index = sum;
+      ans = [element];
+    } else if (sum === index) {
+      ans.push(element);
+    }
+  }
+  return ans;
+};
+
+console.assert(
+  findRestaurant(
+    ["Shogun", "Tapioca Express", "Burger King", "KFC"],
+    [
+      "Piatti",
+      "The Grill at Torrey Pines",
+      "Hungry Hunter Steakhouse",
+      "Shogun",
+    ]
+  ).join() === ["Shogun"].join(),
+  1
+);
+
+console.assert(
+  findRestaurant(
+    ["Shogun", "Tapioca Express", "Burger King", "KFC"],
+    ["KFC", "Shogun", "Burger King"]
+  ).join() === ["Shogun"].join(),
+  2
+);
