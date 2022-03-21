ded2222d6ef28a117f240612d657017a7c06906b
llcoolmxdx5 <2938129812@qq.com>
2022-03-21 22:13:37 +0800

    feat(leetcode): 606-根据二叉树创建字符串

---
+++ 08-算法与数据结构/leetcode/0600-0699/0606. 根据二叉树创建字符串.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {string}
+ */
+var tree2str = function (root) {
+  const dfs = (node) => {
+    if (!node) {
+      return "";
+    }
+    if (!node.left) {
+      return `${node.val}`;
+    }
+    return `${node.val}(${dfs(node.left)})${
+      node.right ? "(" + dfs(node.right) + ")" : ""
+    }`;
+  };
+  return dfs(root);
+};
