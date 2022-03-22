cfb1576aad9c097f752930de831d9c317444f65b
llcoolmxdx5 <2938129812@qq.com>
2022-03-22 22:18:39 +0800

    feat(leetcode): 2038-如果相邻两个颜色均相同则删除当前颜色

---
+++ 08-算法与数据结构/leetcode/2000-2099/2038. 如果相邻两个颜色均相同则删除当前颜色.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} colors
+ * @return {boolean}
+ */
+var winnerOfGame = function (colors) {
+  const { length } = colors;
+  let aCount = 0;
+  let bCount = 0;
+  for (let index = 1; index < length - 1; index++) {
+    if (!colors.slice(index - 1, index + 2).includes('B')) {
+      aCount += 1;
+    }
+    if (!colors.slice(index - 1, index + 2).includes('A')) {
+      bCount += 1;
+    }
+  }
+  // console.log(aCount,bCount);
+  return aCount > bCount;
+};
+
+console.assert(winnerOfGame("AAABABB") === true, 1);
+console.assert(winnerOfGame("AA") === false, 2);
