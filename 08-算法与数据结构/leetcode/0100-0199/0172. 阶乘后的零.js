0b8c2e893773c4e00a6213bcfcca6af0926b94f7
llcoolmxdx5 <2938129812@qq.com>
2022-03-27 11:23:20 +0800

    feat(leetcode): 172-阶乘后的零

---
+++ 08-算法与数据结构/leetcode/0100-0199/0172. 阶乘后的零.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var trailingZeroes = function (n) {
+  let count = 0;
+  const getCount = (num) => {
+    while (num) {
+      if (num % 5 === 0) {
+        count += 1;
+      } else {
+        break;
+      }
+      num /= 5;
+    }
+  };
+  for (let i = 1; i <= n; i++) {
+    getCount(i);
+  }
+  // console.log(count);
+  return count;
+};
+
+var trailingZeroes = function (n) {
+  let ans = 0;
+  for (let i = 5; i <= n; i += 5) {
+    for (let x = i; x % 5 == 0; x /= 5) {
+      ans += 1;
+    }
+  }
+  return ans;
+};
