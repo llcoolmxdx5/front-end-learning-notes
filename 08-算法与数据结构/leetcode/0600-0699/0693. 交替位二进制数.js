fc1a365bce1708d99fdfa2eb0370c4aa499c4fac
llcoolmxdx5 <2938129812@qq.com>
2022-03-28 23:51:53 +0800

    feat(leetcode): 693-交替位二进制数

---
+++ 08-算法与数据结构/leetcode/0600-0699/0693. 交替位二进制数.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var hasAlternatingBits = function (n) {
+  let prev;
+  while (n) {
+    const cur = n % 2;
+    if (prev === cur) {
+      return false;
+    } else {
+      prev = cur;
+    }
+    n = Math.floor(n / 2);
+  }
+  return true
+};
+
+console.assert(hasAlternatingBits(5) === true, 1);
+console.assert(hasAlternatingBits(7) === false, 2);
