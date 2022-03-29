41fe63cfa8fd407fc4fbfbac95336f46ee703d51
llcoolmxdx5 <2938129812@qq.com>
2022-03-29 22:26:05 +0800

    feat(leetcode): 2024-考试的最大困扰度

---
+++ 08-算法与数据结构/leetcode/2000-2099/2024. 考试的最大困扰度.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} answerKey
+ * @param {number} k
+ * @return {number}
+ */
+var maxConsecutiveAnswers = function (answerKey, k) {
+  const n = answerKey.length;
+  const maxConsecutiveChar = (char) => {
+    let ans = 0;
+    for (let left = 0, right = 0, sum = 0; right < n; right++) {
+      sum += answerKey[right] !== char;
+      while (sum > k) {
+        sum -= answerKey[left] !== char;
+        left += 1;
+      }
+      ans = Math.max(ans, right - left + 1);
+    }
+    return ans;
+  };
+  return Math.max(maxConsecutiveChar("T"), maxConsecutiveChar("F"));
+};
+
+console.assert(maxConsecutiveAnswers("TTFF", 2) === 4, 1);
+console.assert(maxConsecutiveAnswers("TTFTTFTT", 1) === 5, 2);
