e627ce81b089bd6288803e3828dd21ea43bf9f48
llcoolmxdx5 <2938129812@qq.com>
2022-03-31 23:55:24 +0800

    feat(leetcode): 727-自除数

---
+++ 08-算法与数据结构/leetcode/0700-0799/0728. 自除数.js
@@ -0,0 +1,35 @@
+/**
+ * @param {number} left
+ * @param {number} right
+ * @return {number[]}
+ */
+var selfDividingNumbers = function (left, right) {
+  const isSelfDividing = (num) => {
+    let temp = num;
+    while (temp) {
+      const digit = temp % 10;
+      if (digit === 0 || num % digit !== 0) {
+        return false;
+      }
+      temp = Math.floor(temp / 10);
+    }
+    return true;
+  };
+  const ans = [];
+  for (let start = left; start <= right; start++) {
+    if (isSelfDividing(start)) {
+      ans.push(start);
+    }
+  }
+  return ans;
+};
+
+console.assert(
+  selfDividingNumbers(1, 22).join() ===
+    [1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 15, 22].join(),
+  1
+);
+console.assert(
+  selfDividingNumbers(47, 85).join() === [48, 55, 66, 77].join(),
+  2
+);
