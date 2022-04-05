e5774ead665de4d5d4f9fe4a13f15634ae89f814
llcoolmxdx5 <2938129812@qq.com>
2022-04-05 22:33:02 +0800

    feat(leetcode): 762-二进制表示中质数个计算置位

---
+++ 08-算法与数据结构/leetcode/0700-0799/0762. 二进制表示中质数个计算置位.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number} left
+ * @param {number} right
+ * @return {number}
+ */
+var countPrimeSetBits = function (left, right) {
+  const primeSet = new Set([2, 3, 5, 7, 11, 13, 17, 19]);
+  let ans = 0;
+  for (let start = left; start <= right; start++) {
+    const bit = start.toString(2);
+    let count = 0;
+    for (const str of bit) {
+      if (str === "1") {
+        count += 1;
+      }
+    }
+    if (primeSet.has(count)) {
+      ans += 1;
+    }
+  }
+  return ans;
+};
+
+console.assert(countPrimeSetBits(6, 10) === 4, 1);
+console.assert(countPrimeSetBits(10, 15) === 5, 1);
