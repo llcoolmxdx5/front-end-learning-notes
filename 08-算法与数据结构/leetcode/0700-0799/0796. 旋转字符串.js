fac5609946e7b3e95e24f71168b8c0f9d6e8024c
llcoolmxdx5 <2938129812@qq.com>
2022-04-07 22:25:09 +0800

    feat(leetcode): 797-旋转字符串

---
+++ 08-算法与数据结构/leetcode/0700-0799/0796. 旋转字符串.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @param {string} goal
+ * @return {boolean}
+ */
+var rotateString = function (s, goal) {
+  if (s.length !== goal.length) return false;
+  if (s === goal) return true;
+  for (let i = 0; i < s.length; i++) {
+    if (s.slice(i + 1) + s.slice(0, i + 1) === goal) {
+      return true;
+    }
+  }
+  return false;
+};
+
+var rotateString = function (s, goal) {
+  return s.length === goal.length && (s + s).indexOf(goal) !== -1;
+};
+
+console.assert(rotateString("abcde", "cdeab") === true, 1);
+console.assert(rotateString("cdeab", "abcde") === true, 2);
+console.assert(rotateString("abcde", "abced") === false, 3);
+console.assert(rotateString("bbbacddceeb", "ceebbbbacdd") === true, 4);
