8f8b532ca3fc310de988e9981953719c9c8bff67
llcoolmxdx5 <2938129812@qq.com>
2022-04-12 13:40:16 +0800

    feat(leetcode): 806-写字符串需要的行数

---
+++ 08-算法与数据结构/leetcode/0800-0899/0806. 写字符串需要的行数.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[]} widths
+ * @param {string} s
+ * @return {number[]}
+ */
+var numberOfLines = function(widths, s) {
+  let line = 1;
+  let rest = 0;
+  for (const str of s) {
+    const number = widths[str.charCodeAt() - 97];
+    if(rest + number > 100) {
+      line += 1;
+      rest = number
+    } else {
+      rest += number
+    }
+  }
+  return [line, rest]
+};
+
+console.assert(
+  numberOfLines(
+    [10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10]
+    ,"abcdefghijklmnopqrstuvwxyz"
+  ).join() === [3, 60].join(),
+  1
+);
+
+console.assert(
+  numberOfLines(
+    [4,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10],
+    "bbbcccdddaaa").join() === [2, 4].join(),
+  2
+);
