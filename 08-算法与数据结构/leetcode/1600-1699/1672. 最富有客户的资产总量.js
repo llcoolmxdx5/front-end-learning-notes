b63ceec092657fa72de1f3ca61d02f6d20c76a5b
llcoolmxdx5 <2938129812@qq.com>
2022-04-14 19:04:53 +0800

    feat(leetcode): 1672-最富有客户的资产总量

---
+++ 08-算法与数据结构/leetcode/1600-1699/1672. 最富有客户的资产总量.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[][]} accounts
+ * @return {number}
+ */
+var maximumWealth = function (accounts) {
+  let sum = 0;
+  for (let i = 0; i < accounts.length; i++) {
+    let temp = 0;
+    for (let j = 0; j < accounts[0].length; j++) {
+      temp += accounts[i][j];
+    }
+    sum = Math.max(sum, temp);
+  }
+  return sum;
+};
+
+console.assert(
+  maximumWealth([
+    [1, 2, 3],
+    [3, 2, 1],
+  ]) === 6,
+  1
+);
