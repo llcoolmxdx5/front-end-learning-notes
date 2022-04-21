432cdd2543fb168ce515c7827acb384fc4452d36
llcoolmxdx5 <2938129812@qq.com>
2022-04-21 10:33:11 +0800

    feat(leetcode): 824-山羊拉丁文

---
+++ 08-算法与数据结构/leetcode/0800-0899/0824. 山羊拉丁文.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} sentence
+ * @return {string}
+ */
+var toGoatLatin = function (sentence) {
+  const arr = sentence.split(" ");
+  const ans = [];
+  let index = 1;
+  for (const str of arr) {
+    if (["a", "e", "i", "o", "u"].includes(str[0].toLowerCase())) {
+      ans.push(str + "ma" + "a".repeat(index));
+    } else {
+      ans.push(str.slice(1) + str[0] + "ma" + "a".repeat(index));
+    }
+    index += 1;
+  }
+  // console.log(ans);
+  return ans.join(" ");
+};
+
+console.assert(
+  toGoatLatin("The quick brown fox jumped over the lazy dog") ===
+    "heTmaa uickqmaaa rownbmaaaa oxfmaaaaa umpedjmaaaaaa overmaaaaaaa hetmaaaaaaaa azylmaaaaaaaaa ogdmaaaaaaaaaa",
+  1
+);
