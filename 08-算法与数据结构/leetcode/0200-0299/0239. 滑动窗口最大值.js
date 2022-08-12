8e5acdce1837773b4bf0235625fc745062fb2e0c
llcoolmxdx5 <2938129812@qq.com>
2022-08-12 21:02:14 +0800

    feat(leetcode): 239-滑动窗口最大值

---
+++ 08-算法与数据结构/leetcode/0200-0299/0239. 滑动窗口最大值.js
@@ -0,0 +1,63 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var maxSlidingWindow = function (nums, k) {
+  const arr = nums.slice(0, k);
+  const res = [Math.max(...arr)];
+  for (let i = k; i < nums.length; i++) {
+    arr.shift();
+    arr.push(nums[i]);
+    res.push(Math.max(...arr));
+  }
+  return res;
+};
+
+import PriorityQueue from '../../datastructure/Queue/PriorityQueue';
+
+var maxSlidingWindow = function (nums, k) {
+  const priorityQueue = new PriorityQueue((a, b) => a[0] > b[0]);
+  for (let i = 0; i < k; i++) {
+    priorityQueue.offer([nums[i], i]);
+  }
+  const res = [priorityQueue.peek()[0]];
+  for (let i = k; i < nums.length; i++) {
+    priorityQueue.offer([nums[i], i]);
+    while (priorityQueue.peek()[1] <= i - k) {
+      priorityQueue.poll();
+    }
+    res.push(priorityQueue.peek()[0]);
+  }
+  // console.log(res);
+  return res;
+};
+
+var maxSlidingWindow = function (nums, k) {
+  const n = nums.length;
+  const q = [];
+  for (let i = 0; i < k; i++) {
+    while (q.length && nums[i] >= nums[q.at(-1)]) {
+      q.pop();
+    }
+    q.push(i);
+  }
+
+  const ans = [nums[q[0]]];
+  for (let i = k; i < n; i++) {
+    while (q.length && nums[i] >= nums[q.at(-1)]) {
+      q.pop();
+    }
+    q.push(i);
+    while (q[0] <= i - k) {
+      q.shift();
+    }
+    ans.push(nums[q[0]]);
+  }
+  return ans;
+};
+
+console.assert(
+  maxSlidingWindow([1, 3, -1, -3, 5, 3, 6, 7], 3).join() === [3, 3, 5, 5, 6, 7].join(),
+  1,
+);
